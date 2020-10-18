8c894d232102698d071545d76aa7923fc34f005c
santiago bianco <santiago.bianco91@gmail.com>
2020-10-18 17:12:30 -0300

    cors permito todo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,17 @@
 const { Router } = require("express");
 const router = Router();
 var request = require("request");
+var cors =  require("cors")
+
+
+router.use(cors());
+router.options('*',cors());
 
 router.get("/", (req, res) => {
   res.send("Bienvenido a airbnb bussines core");
 });
 
-router.get("/profile-status", (req, res) => {
+router.get("/profile-status", (req, res,next) => {
   request.get(
     "https://taller2airbnb-profile.herokuapp.com/health.json",
     (error, response, body) => {
