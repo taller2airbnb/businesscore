75f59c7d52a24ca4df35c627190ddb50a4fd0f23
santiago bianco <santiago.bianco91@gmail.com>
2020-10-18 16:17:50 -0300

    health rest

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,26 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
+var https = require("https");
+var request = require("request");
 
-router.get('/', (req, res) => {
-    res.send('Bienvenido a airbnb bussines core');
+router.get("/", (req, res) => {
+  res.send("Bienvenido a airbnb bussines core");
+});
+
+router.get("/profile-status", (req, res) => {
+  request.get(
+    "https://taller2airbnb-profile.herokuapp.com/health.json",
+    (error, response, body) => {
+      if (error) {
+        return console.dir(error);
+      }
+      res.send(JSON.parse(body).status);
+    }
+  );
+});
+
+router.get("/health", (req, res) => {
+  res.send(JSON.stringify({ status: "UP" }));
 });
 
 module.exports = router;
