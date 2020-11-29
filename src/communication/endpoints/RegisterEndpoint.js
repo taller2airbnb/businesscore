43e84ef9e560258b75fece59aaf5ee8723d89e2d
santiago.bianco <santiago.bianco91@gmail.com>
2020-11-28 22:25:18 -0300

    hot fix, cambio el endpoint de regiter

--- src/communication/endpoints/RegisterEndpoint.js
+++ src/communication/endpoints/RegisterEndpoint.js
@@ -6,7 +6,7 @@ const InvalidRegister = require("../responses/user/InvalidRegister");
 
 module.exports = class RegisterAdminEndpoint extends Endpoint {
   static url() {
-    return "/register_admin/";
+    return "/user/";
   }
 
   method() {
