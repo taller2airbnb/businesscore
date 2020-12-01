f5b7bf57221cefdf227dcc3f121d572e85b6371a
santiago bianco <santiago.bianco91@gmail.com>
2020-12-01 00:33:12 -0300

    el get de features devuelve una respuesta estandarizada

--- src/routes/posting.js
+++ src/routes/posting.js
@@ -2,10 +2,10 @@ const { Router } = require("express");
 const router = Router();
 var cors = require("cors");
 router.use(cors());
-router.options('*', cors());
+router.options("*", cors());
 const dao = require("../db/index");
-var validToken = require('./tokenController.js');
-var decodeToken = require('./tokenController.js');
+var validToken = require("./tokenController.js");
+var decodeToken = require("./tokenController.js");
 
 /**
  * @swagger
@@ -28,13 +28,16 @@ var decodeToken = require('./tokenController.js');
 router.get("/posting", async (req, res) => {
   if (!validToken.validToken(req, res)) return;
   const future = dao.execSql("get_posting", [req.query.idPosting]);
-  future.then(result => {
-    res.status(200).send({ "message": result, "status": 200, "error": false });
-  }).catch(error => {
-    res.status(500).send({ "message": "Data base: " + error, "status": 500, "error": true });
-  });
+  future
+    .then((result) => {
+      res.status(200).send({ message: result, status: 200, error: false });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send({ message: "Data base: " + error, status: 500, error: true });
+    });
 });
-
 
 /**
  * @swagger
@@ -63,17 +66,26 @@ router.get("/posting", async (req, res) => {
 router.post("/posting", async (req, res) => {
   //if (!validToken.validToken(req, res)) return;
   //let tokenDecode = decodeToken.decodeToken(req);
-  const future = dao.execSql("create_posting", [req.body.price_day,
-  req.body.start_date, req.body.end_date, req.body.state,
-  req.body.features, req.body.public, req.body.content, 1]);
-  future.then(result => {
-    res.status(200).send({ "message": result, "status": 200, "error": false });
-  }).catch(error => {
-    res.status(500).send({ "message": "Data base: " + error, "status": 500, "error": true });
-  });
-
+  const future = dao.execSql("create_posting", [
+    req.body.price_day,
+    req.body.start_date,
+    req.body.end_date,
+    req.body.state,
+    req.body.features,
+    req.body.public,
+    req.body.content,
+    1,
+  ]);
+  future
+    .then((result) => {
+      res.status(200).send({ message: result, status: 200, error: false });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send({ message: "Data base: " + error, status: 500, error: true });
+    });
 });
-
 
 /**
  * @swagger
@@ -106,14 +118,25 @@ router.post("/posting", async (req, res) => {
  */
 router.put("/posting/:idPosting", async (req, res) => {
   if (!validToken.validToken(req, res)) return;
-  const future = dao.execSql("update_posting", [req.params.idPosting, req.body.price_day,
-  req.body.start_date, req.body.end_date, req.body.state,
-  req.body.features, req.body.public, req.body.content]);
-  future.then(result => {
-    res.status(200).send({ "message": result, "status": 200, "error": false });
-  }).catch(error => {
-    res.status(500).send({ "message": "Data base: " + error, "status": 500, "error": true });
-  });
+  const future = dao.execSql("update_posting", [
+    req.params.idPosting,
+    req.body.price_day,
+    req.body.start_date,
+    req.body.end_date,
+    req.body.state,
+    req.body.features,
+    req.body.public,
+    req.body.content,
+  ]);
+  future
+    .then((result) => {
+      res.status(200).send({ message: result, status: 200, error: false });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send({ message: "Data base: " + error, status: 500, error: true });
+    });
 });
 
 /**
@@ -122,7 +145,7 @@ router.put("/posting/:idPosting", async (req, res) => {
  *   delete:
  *     tags:
  *       - posting
- *     description: delete posting 
+ *     description: delete posting
  *     produces:
  *       - application/json
  *     security:
@@ -142,13 +165,16 @@ router.put("/posting/:idPosting", async (req, res) => {
 router.delete("/posting/:idPosting", async (req, res) => {
   if (!validToken.validToken(req, res)) return;
   const future = dao.execSql("delete_posting", [req.params.idPosting]);
-  future.then(result => {
-    res.status(200).send({ "message": result, "status": 200, "error": false });
-  }).catch(error => {
-    res.status(500).send({ "message": "Data base: " + error, "status": 500, "error": true });
-  });
+  future
+    .then((result) => {
+      res.status(200).send({ message: result, status: 200, error: false });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send({ message: "Data base: " + error, status: 500, error: true });
+    });
 });
-
 
 /**
  * @swagger
@@ -189,13 +215,22 @@ router.delete("/posting/:idPosting", async (req, res) => {
  */
 router.get("/posting/search", async (req, res) => {
   // if (!validToken.validToken(req, res)) return;
-  const future = dao.execSql("search_posting", [req.query.priceMin,
-  req.query.priceMax, req.query.startDate, req.query.endDate, req.query.feature]);
-  future.then(result => {
-    res.send({ "message": result, "status": 200, "error": false });
-  }).catch(error => {
-    res.status(500).send({ "message": "Data base: " + error, "status": 500, "error": true });
-  });
+  const future = dao.execSql("search_posting", [
+    req.query.priceMin,
+    req.query.priceMax,
+    req.query.startDate,
+    req.query.endDate,
+    req.query.feature,
+  ]);
+  future
+    .then((result) => {
+      res.send({ message: result, status: 200, error: false });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send({ message: "Data base: " + error, status: 500, error: true });
+    });
 });
 
 /**
@@ -214,11 +249,15 @@ router.get("/posting/search", async (req, res) => {
 router.get("/feature", async (req, res) => {
   //if (!validToken.validToken(req, res)) return;
   const future = dao.execSql("get_feature");
-  future.then(result => {
-    res.send(JSON.stringify(result));
-  }).catch(error => {
-    res.status(500).send("Data base: " + error);
-  });
+  future
+    .then((result) => {
+      res.send({ message: result, status: 200, error: false });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .send({ message: "Data base: " + error, status: 500, error: true });
+    });
 });
 
 module.exports = router;
