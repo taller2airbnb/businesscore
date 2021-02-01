c1f3afc27d496997a17e6e03136233be0a8062e4
santiago.bianco <santiago.bianco91@gmail.com>
2021-02-01 13:40:27 -0300

    accept booking test

--- test/routes/booking.test.js
+++ test/routes/booking.test.js
@@ -94,15 +94,6 @@ describe(" Test Suite: Bookings", () => {
         error: true
       });
 
-      daoMock.mockResolvedValueOnce([{
-        "id_booking": 40,
-        "state_operation": "INTENT_BOOKING",
-        "check_in": "2028-08-08T00:00:00.000Z",
-        "check_out": "2028-08-09T00:00:00.000Z",
-        "transaction_hash_intent": "0x9e542505b6fe93777e427b8f39ce298419f50df4417b9319d66ef5228a8feed9",
-        "id_user": 3,
-        "id_posting": 17
-      }]);
 
       const res = await request.post("/intentBooking").send({
         "idPosting": 17,
@@ -174,10 +165,6 @@ describe(" Test Suite: Bookings", () => {
       tokenDecodeMock.mockReturnValue({ payload: { id: 8 } });
 
       const daoMock = jest.spyOn(dao, "execSql");
-      daoMock.mockImplementation(() => {
-        throw "un error mockeado";
-      });
-
 
       const res = await request.get("/myOffers").send();
       expect(res.body.status).toBe(500);
@@ -187,5 +174,97 @@ describe(" Test Suite: Bookings", () => {
       console.log(error.message);
     }
   });
+
+  it("Accept booking", async () => {
+    try {
+      const tokenDecodeMock = jest.spyOn(decodeToken, "decodeToken");
+      tokenDecodeMock.mockReturnValue({ payload: { id: 8 } });
+
+      const daoMock = jest.spyOn(dao, "execSql");
+      daoMock.mockResolvedValueOnce([{
+        creator_id_booker: 16,
+        initial_day: 4,
+        initial_month: 11,
+        initial_year: 2022,
+        last_day: 8,
+        last_month: 11,
+        last_year: 2022,
+        transaction_room: "0xdbb8fe79357a6816287057f39b9533a13e97438908a2266a1fff9f0b52d5171f",
+      }]);
+
+      const apiClientMock = jest.spyOn(apiClient.prototype, "acceptBooking");
+      apiClientMock.mockReturnValue({
+        status: 200,
+        message: {
+          bookingTransactionHash: "0x15fb9cdfbfb17426de4f906bf75c7e8cf6b7185635ce488dccd9de02b8d01cb5",
+        },
+        error: false,
+      });
+
+      daoMock.mockResolvedValueOnce([{}]);
+      const res = await request.post("/acceptBooking").send({
+        "transactionHash": "0xc41adbc118cb0cdf7f086a9f7baacd84442455e00e215d98140a2c92e87b344f"
+      });
+      expect(res.body.status).toBe(200);
+      expect(res.body.error).toBe(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+
+  it("Accept booking fail smart contract", async () => {
+    try {
+      const tokenDecodeMock = jest.spyOn(decodeToken, "decodeToken");
+      tokenDecodeMock.mockReturnValue({ payload: { id: 8 } });
+
+      const daoMock = jest.spyOn(dao, "execSql");
+      daoMock.mockResolvedValueOnce([{
+        creator_id_booker: 16,
+        initial_day: 4,
+        initial_month: 11,
+        initial_year: 2022,
+        last_day: 8,
+        last_month: 11,
+        last_year: 2022,
+        transaction_room: "0xdbb8fe79357a6816287057f39b9533a13e97438908a2266a1fff9f0b52d5171f",
+      }]);
+
+      const apiClientMock = jest.spyOn(apiClient.prototype, "acceptBooking");
+      apiClientMock.mockReturnValue({
+        status: 401,
+        message: "Insuficient money for transaction",
+        error: true
+      });
+
+      const res = await request.post("/acceptBooking").send({
+        "transactionHash": "0xc41adbc118cb0cdf7f086a9f7baacd84442455e00e215d98140a2c92e87b344f"
+      });
+      expect(res.body.status).toBe(401);
+      expect(res.body.error).toBe(true);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+
+  it("Accept booking fail ", async () => {
+    try {
+      const tokenDecodeMock = jest.spyOn(decodeToken, "decodeToken");
+      tokenDecodeMock.mockReturnValue({ payload: { id: 8 } });
+
+      const daoMock = jest.spyOn(dao, "execSql");
+      daoMock.mockImplementation(() => {
+        throw "un error mockeado";
+      });
+
+      const res = await request.post("/acceptBooking").send({
+        "transactionHash": "0xc41adbc118cb0cdf7f086a9f7baacd84442455e00e215d98140a2c92e87b344f"
+      });
+      expect(res.body.status).toBe(500);
+      expect(res.body.error).toBe(true);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+
 
 });
